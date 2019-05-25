37e95ed6c5d7268dc4e8bb11fa0e97b80d9953b3
Abraham Correa <abrancorrea@outlook.com>
2019-05-24 23:04:22 -0400

    Added Logos and edited Footer and a few words

--- js/portafolio.js
+++ js/portafolio.js
@@ -23,7 +23,7 @@ new Vue ({
                 title: 'Control Admin System for College',
                 description: `
                 As part, of my internships, i maded a project to keep control to multiple
-                behaviors that my College coordinacion needs. This necesidades includes
+                needs that my College coordinacion wants. This needs includes
                 control of asistence, upload notas, register students, send mails automatically,
                 and generate diferents documents about control. 
                 The project is actually working in the College intranet around the country.
@@ -125,5 +125,5 @@ jQuery('div.projects').waypoint(function(){
 jQuery('div.skills').waypoint(function(){
     jQuery('div.skills').children('h1').addClass('fadeIn');
     jQuery('.skills2').addClass('fadeInDown');
-    jQuery('.skillsdiv').addClass('fadeInDown');
+    jQuery('.skillslogos').addClass('fadeInDown');
 },{offset: '100%'});
